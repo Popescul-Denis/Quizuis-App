d0d6d0056aefc8f3ff573a2a0fc85e024a051719
Popescul-Denis <popescu.personal16@gmail.com>
2026-04-26 12:54:49 +0300

    Sper ca este ultima

--- app/api/user/username/[username]/route.ts
+++ app/api/user/username/[username]/route.ts
@@ -3,7 +3,7 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/prisma";
 
-export async function GET(req : NextRequest, { params }: { params: { username: string } }) {
+export async function GET(req : NextRequest, { params }: { params: Promise<{ username: string }> }) {
   try{
     const session = await getServerSession(authOptions);
     if(!session?.user?.email){
